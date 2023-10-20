f217818a6eebe24cbc5a3f6c32b3c46b4e98e0d2
a-oshea <tony.oshea@gmail.com>
2023-10-20 11:22:13 -0400

    adding managers

--- fasting_app/fasting_tracker/models.py
+++ fasting_app/fasting_tracker/models.py
@@ -6,6 +6,8 @@ from model_utils.models import TimeStampedModel
 
 from fasting_app.core.models import AuditableModel
 
+from fasting_app.fasting_app.fasting_tracker.managers import FastingSessionManager
+
 
 class FastingSession(AuditableModel, TimeStampedModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'), related_name='fasting_sessions',
@@ -15,3 +17,5 @@ class FastingSession(AuditableModel, TimeStampedModel):
     duration = models.FloatField(_('Duration'), default=0)
     target_duration = models.PositiveSmallIntegerField(_('Target duration'), default=16)
     comments = models.CharField(_('Comments'), max_length=180, null=True, blank=True)
+
+    objects = FastingSessionManager()
